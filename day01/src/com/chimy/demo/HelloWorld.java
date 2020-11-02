1148fff83bd4188f6f25f4aacb0b8ab2a738824b
chimy <chimywang@163.com>
2020-11-02 14:30:34 +0800

    -

---
+++ day01/src/com/chimy/demo/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.chimy.demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ day01/src/com/chimy/varable/Demo1.java
@@ -0,0 +1,10 @@
+package com.chimy.varable;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        a = 20;
+        System.out.println(a);
+    }
+}
